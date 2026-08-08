9a878704a2f19ad76e1a2848c40d11970641589e
Daksh Solanki <solankidaksh97@gmail.com>
2026-08-08 13:41:46 +0530

    new

--- 1_agent_loop_langchain_tool_calling.py
+++ 1_agent_loop_langchain_tool_calling.py
@@ -7,6 +7,7 @@ from langchain.tools import tool
 from langchain.messages import HumanMessage, SystemMessage, ToolMessage
 from langsmith import traceable
 from langchain_tavily import TavilySearch
+from langchain.prompts import PromptTemplate
 
 MAX_ITERATIONS = 10
 MODEL = "qwen3.5:72b"
