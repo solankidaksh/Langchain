d1a01c69f327d4aae5aedd0d949887c85cdb9f63
Daksh Solanki <solankidaksh97@gmail.com>
2026-08-27 22:55:48 +0530

    Update

--- practice.py
+++ practice.py
@@ -1,13 +1,16 @@
-from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate, prompt
 from langchain_ollama import ChatOllama
+from urllib3 import response
 
 
-template = PromptTemplate.from_template(f"""Answer the question based on the context below. If the question cannot be answered using the information provided, answer with "I dont know"
+prompt = PromptTemplate.from_template(f"""Answer the question based on the context below. If the question cannot be answered using the information provided, answer with "I dont know"
 Context: {context}
 Question: {question}
 Answer: """)
 
-template.invoke({
+model = ChatOllama(model="gemma4:e2b ")
+chain = prompt | model
+response = prompt.invoke({
     "context": f""" The most recent advancements in NLP are being driven by Large Language Models(LLMs). 
     These models outperform their smaller counterparts and hae become invaluable for developers who are creating 
     application with NLP capabilities. Developers can tap into these models through Hugging Face's transformers' library, or by utilizing
