180ca94cc93b4cdd4695789a018e1ee2038786ac
Daksh Solanki <solankidaksh97@gmail.com>
2026-09-02 17:07:39 +0530

    Updated

--- rag.py
+++ rag.py
@@ -1,4 +1,11 @@
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 loader = PyPDFLoader("./practice.pdf")
 pages = loader.load()
+
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 1000,
+    chunk_overlap = 200
+)
+splitted_pages = splitter.split_documents(pages)
