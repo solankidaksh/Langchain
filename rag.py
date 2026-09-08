ac5227558a1c08e04e371f9600b2d27c143f087a
Daksh Solanki <solankidaksh97@gmail.com>
2026-09-08 23:49:46 +0530

    Updated

--- rag.py
+++ rag.py
@@ -2,8 +2,8 @@ from langchain_community.document_loaders import TextLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 
-loader = PyPDFLoader("./practice.pdf")
-pages = loader.load()
+loader = TextLoader("./test.txt")
+doc = loader.load()
 
 splitter = RecursiveCharacterTextSplitter(
     chunk_size = 1000,
