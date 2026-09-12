107d8d1179923f098f6706e31fe5e3e345a77058
Daksh Solanki <solankidaksh97@gmail.com>
2026-09-12 12:08:51 +0530

    Learning pgvectore database

--- pg_vector.py
+++ pg_vector.py
@@ -5,6 +5,8 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.documents import Document
 import uuid
 
+from rag import connection
+
 connnection = 'postgressql+psycopg://langchain:langchain@localhost:6024/langchain'
 
 raw_documents = TextLoader('./test.txt').load()
@@ -12,3 +14,9 @@ text_splitter = RecursiveCharacterTextSplitter(
     chunk_size=1000, chunk_overlap=200
 )
 documents = text_splitter.split_documents(raw_documents)
+
+model = HuggingFaceEmbeddings(model = "sentence-transformers/all-MiniLM-L6-v2")
+
+db = PGVectorStore.from_documents(
+    documents, model, connection=connection)
+db.similarity_search("what is captial of India?", k=4)
